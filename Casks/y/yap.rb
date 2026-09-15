5bedbe6bd097471425c463daa647dc51aecdba46
christianmat <christian@frigade.com>
2026-09-14 18:05:09 -0700

    Update Yap to 0.1.12

--- Casks/y/yap.rb
+++ Casks/y/yap.rb
@@ -1,9 +1,8 @@
 cask "yap" do
-  version "0.1.11"
-  sha256 "c89e5a7774e9055caef11894ce281ded19a2a223ec1f1813936381b20424c20b"
+  version "0.1.12"
+  sha256 "c600cad9120cbd7ec463d001f002ba3e6cfdf2445577cb957d4166d62824b28e"
 
-  url "https://github.com/FrigadeHQ/yap/releases/download/v#{version}/Yap.dmg",
-      verified: "github.com/FrigadeHQ/yap/"
+  url "https://github.com/FrigadeHQ/yap/releases/download/v#{version}/Yap.dmg"
   name "Yap"
   desc "On-device voice dictation"
   homepage "https://frigade.com/yap"
@@ -13,8 +12,8 @@ cask "yap" do
     strategy :github_latest
   end
 
-  depends_on macos: :tahoe
   depends_on arch: :arm64
+  depends_on macos: :tahoe
 
   app "Yap.app"
 
